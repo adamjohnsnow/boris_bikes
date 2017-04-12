d0b86eafd574292523b187972e3f5c34cfa9b767
JayWebDevCom <jaiye.s@gmail.com>
2017-04-12 16:44:29 +0100

    Add ex.11 Instance Variables

--- lib/boris-bikes.rb
+++ lib/boris-bikes.rb
@@ -1,10 +1,22 @@
 class DockingStation
+
   def release_bike
     Bike.new
   end
+
+  def dock_bike(bike)
+    @bike = bike
+  end
+
+  def bike
+    @bike
+  end
+
 end
 
 class Bike
+
   def working?
   end
+
 end
--- spec/boris-bikes_spec.rb
+++ spec/boris-bikes_spec.rb
@@ -1,16 +1,30 @@
 require 'boris-bikes'
 
-
-
 describe DockingStation do
 
   dockingstation = DockingStation.new
   bike = dockingstation.release_bike
 
-  it 'Docking station respondes to :release_bike' do
+  it 'Docking station responds to :release_bike' do
     expect(dockingstation).to respond_to :release_bike
   end
+
   it 'Docking station repsonds_to to :working' do
     expect(bike).to respond_to :working?
   end
+
+  it 'Docking station responds to :release_bike' do
+    expect(dockingstation).to respond_to(:dock_bike).with(1).argument
+  end
+
+  it 'responds to #bike' do
+    expect(dockingstation).to respond_to(:bike)
+  end
+
+  it 'returns bike instance on #bike' do
+    dockingstation.dock_bike(bike)
+    expect(dockingstation.bike).to eq bike
+  end
+
+
 end
