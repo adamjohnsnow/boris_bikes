b4323091ba8da8a4126fd3f6cd37eec9c4d25508
JayWebDevCom <jaiye.s@gmail.com>
2017-04-12 13:24:08 +0100

    Added the Bike class within DockingStation.release_bike and feature tested

--- spec/boris-bikes_spec.rb
+++ spec/boris-bikes_spec.rb
@@ -3,8 +3,14 @@ require 'boris-bikes'
 
 
 describe DockingStation do
-  it do
-    dockingstation = DockingStation.new
+
+  dockingstation = DockingStation.new
+  bike = dockingstation.release_bike
+
+  it 'Docking station respondes to :release_bike' do
     expect(dockingstation).to respond_to :release_bike
+  end
+  it 'Docking station repsonds_to to :working' do
+    expect(bike).to respond_to :working?
   end
 end
